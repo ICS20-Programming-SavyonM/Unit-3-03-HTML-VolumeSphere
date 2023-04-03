815047ef54cb21611eb9080a785d24c1c5a8a8c1
SavyonM <124379798+SavyonMalko@users.noreply.github.com>
2023-04-03 22:07:28 +0000

    now done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+// Copyright (c) 2023 Savyon All rights reserved
+//
+// Created by: Savyon
+// Created on: March 2023
+// This file contains the JS functions for index.html
 
 "use strict"
 
+//calculate volume 
 function calculate() {
   var radius = document.getElementById("radius").value;
   var volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById("result").value = volume.toFixed(2);
+  document.getElementById("result").innerHTML = "The volume is " + volume.toFixed(2) + "m<sup>3</sup>"
 }
